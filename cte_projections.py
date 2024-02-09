269a9189dcd82269b717c928a4318009359ef846
OrangeAed <ajsimonsrudolph@gmail.com>
2024-02-09 10:37:11 -0500

    Current work on python job projections

--- cte_projections.py
+++ cte_projections.py
@@ -15,12 +15,12 @@ class CTE_Projections:
             print(pathways)
         return projection_df, pathways
 
-
-
     def total_jobs(self, is_current: bool):
         self.cte_jobs = {}
         for cte_pathway in self.pathways_data:
-            self.cte_jobs[cte_pathway] = self.projection_df[self.projection_df["Industry Title"] in self.pathways_data[cte_pathway]["jobs"]]["2021"]
+            jobs = self.pathways_data[cte_pathway]["jobs"]
+            relevant_job_rows = self.projection_df[self.projection_df["Industry Title"] in jobs]
+            self.cte_jobs[cte_pathway] = sum(relevant_job_rows["2021"])
 
         return self.cte_jobs
 
